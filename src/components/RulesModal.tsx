d97b865b3b2cb29994ed03ee070f2873adb59a7a
AlexandreGaultier <alexandre.gaultier44@gmail.com>
2024-11-06 23:05:55 +0100

    try to fix building bug

--- src/components/RulesModal.tsx
+++ src/components/RulesModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef } from 'react';
 import styles from './RulesModal.module.css';
 import { CLASSES } from '../data/classes';
 
